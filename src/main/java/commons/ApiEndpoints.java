88ed5ed7f5c5fed7a2fdfe77e2acef8ec9379630
ADMIN <nguyenthanhtinh97@gmail.com>
2025-04-19 10:07:14 +0700

    update structure

---
+++ src/main/java/commons/ApiEndpoints.java
@@ -0,0 +1,6 @@
+package commons;
+
+public class ApiEndpoints {
+    public static final String LOGIN            = "/api/users/auth-login?format=json";
+    public static final String CURRENT_SESSION  = "/api/retailers/currentsession?format=json";
+}
